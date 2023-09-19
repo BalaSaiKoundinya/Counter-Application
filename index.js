c4dcc126854d151a8c10cd5d6f7215de65c4bad8
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 18:12:14 +0530

    Changed the colors of numbers according to their state

--- index.js
+++ index.js
@@ -5,15 +5,36 @@ function onIncrement() {
     let previousCounterValue = counterElement.textContent;
     let updatedCounterValue = parseInt(previousCounterValue) + 1;
     counterValue.textContent = updatedCounterValue;
+
+    if (updatedCounterValue>0) {
+        counterElement.style.color = "Green";
+    } 
+    else if (updatedCounterValue<0){
+        counterElement.style.color = "red";
+    }
+    else {
+        counterElement.style.color = "blue"
+    }
 }
 
 function onDecrement() {
     let previousCounterValue = counterElement.textContent;
     let updatedCounterValue = parseInt(previousCounterValue) - 1;
     counterValue.textContent = updatedCounterValue;
+
+    if (updatedCounterValue>0) {
+        counterElement.style.color = "Green";
+    } 
+    else if (updatedCounterValue<0){
+        counterElement.style.color = "red";
+    }
+    else {
+        counterElement.style.color = "blue"
+    }
 }
 
 function onReset() {
     let updatedCounterValue = 0;
     counterValue.textContent = updatedCounterValue;
+    counterElement.style.color = "blue";
 }
