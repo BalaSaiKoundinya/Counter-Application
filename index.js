6421dffd0cf8949c0dbed731c27a1063f5b3f266
BalaSaiKoundinya <127856753+BalaSaiKoundinya@users.noreply.github.com>
2023-09-19 17:58:09 +0530

    Modified the Increment, Decrement and Reset buttons and changed their status dynamically

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ let counterElement = document.getElementById("counterValue");
 
 function onIncrement() {
     let previousCounterValue = counterElement.textContent;
-    let updatedCounterValue = previousCounterValue + 1;
-    console.log(updatedCounterValue);
+    let updatedCounterValue = parseInt(previousCounterValue) + 1;
+    counterValue.textContent = updatedCounterValue;
 }
 
 function onDecrement() {
-
+    let previousCounterValue = counterElement.textContent;
+    let updatedCounterValue = parseInt(previousCounterValue) - 1;
+    counterValue.textContent = updatedCounterValue;
 }
 
 function onReset() {
-
+    let updatedCounterValue = 0;
+    counterValue.textContent = updatedCounterValue;
 }
